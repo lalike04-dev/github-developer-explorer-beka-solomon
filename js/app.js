54289986ec5ff254e6b81bba3d64c171fd47e55f
lalike04-dev <lalike04@gmail.com>
2026-08-10 16:46:55 +0300

    setting the app.js file for operations by adding the functions and featurs

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 import { fetchGithubUser,fetchUserRepos } from "./githubApi.js";
 import { repoFilters,repoStats,displayInformation } from "./analytic.js";
 
+//here is how to fetch user and their repo data from github
+fetchGithubUser("octocat")
+fetchUserRepos("octocat")
+console.log(fetchGithubUser("octocat"))
+console.log(fetchUserRepos("octocat"))
+//here is how to display information of a selected user
+displayInformation("octocat")
