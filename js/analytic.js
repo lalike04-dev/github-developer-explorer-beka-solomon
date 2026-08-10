ee3b49ba057a4a9d38397573ca9da964075751b0
lalike04-dev <lalike04@gmail.com>
2026-08-10 16:04:26 +0300

    Adding repo statictics into the program

--- js/analytic.js
+++ js/analytic.js
@@ -28,4 +28,65 @@ export async function displayInformation(username) {
 }
 //displayInformation("to");
 
-exoprt async function 
+export async function repoStats(username){
+    if(username==null || username==undefined){
+        console.log("Please enter a User name!")
+    }
+    else{
+        try{
+            const userRepoInfo = await fetchUserRepos(username);
+            if(!userRepoInfo.ok){
+               switch(userRepoInfo.status){
+            case 404: throw new Error (`User Not Found!`)
+            break;
+            case 429: throw new Error (`Too many tries!`)
+            break;
+            case 403: throw new Error (`Account is Forbidden to search!`)
+            break;
+            default:
+                break; 
+            }
+        }
+        const repocount=userRepoInfo.length;
+        const starcount=userRepoInfo.reduce((accumulator, value)=>{
+            return accumulator + value.stargazers_count
+        },0);
+        const forkcount=userRepoInfo.reduce((accumulator, value)=>{
+            return accumulator + value.forks_count
+        },0);
+        const moststarred=[...userRepoInfo].sort((a,b)=>b?.stargazers_count-a?.stargazers_count);
+        
+        const languages=[];
+        const languagesCount= new Map()
+
+        for(let i=0;i<userRepoInfo.length;i++){
+            if(!languages.includes(userRepoInfo[i]?.language)){
+                languages.push(userRepoInfo[i].language)
+            }}
+        for(let i=0;i<userRepoInfo.length;i++){
+            let count=0;
+            for(let j=0;j<languages.length;j++){
+                if(userRepoInfo[i].language==languages[j])
+                    count++;
+            }
+                languagesCount.set(userRepoInfo[i].language,count);
+        }
+        console.log("Repo REport\n")
+        console.log(`repo count:${repocount}`)
+        console.log(`star count:${starcount}`)
+        console.log(`fork count:${forkcount}`)
+        console.log(`most starred repos from 1-5`)
+        for(let i=0;i<5;i++)
+            console.log(`${i+1})${moststarred[i].name}`);
+        console.log(`languages available in the repos and their count`)
+        languagesCount.forEach((value, key) => {
+                console.log(`${key}: ${value}`);
+                });
+        
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+}
+repoStats("octocat");
